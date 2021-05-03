ea53468d2e33ed40a3ed66ee592ea44b1f46546d
Jeremy Kepf <jeremy1975@gmail.com>
2021-05-03 16:03:07 -0400

    golf pdf

--- src/components/GolfSections.js
+++ src/components/GolfSections.js
@@ -4,7 +4,7 @@ import golf1 from '../img/golf1.jpg';
 import golf3 from '../img/hickoryWoods.jpg';
 
 import homeSection2pic from '../img/homeSection2pic.jpg';
-import golfPDF from '../img/golf2021.pdf';
+import golfPDF from '../img/2021golf.pdf';
 
 
 import '../App.css';
