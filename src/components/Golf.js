1874d95208bd9e2a26df6380adae9790dc100ff1
Jeremy Kepf <jeremy1975@gmail.com>
2021-05-27 13:06:55 -0400

    changed golf to sold out and changed benefit pricing, added banner to golf page

--- src/components/Golf.js
+++ src/components/Golf.js
@@ -8,6 +8,10 @@ class Golf extends Component {
   render() {
     return <div className="Golf">
     <Navbar />
+    <div className="Covid-Banner">
+      <p><b>We are sold out! Thank you everyone for such a great response to this year's event!</b></p>
+      
+    </div>
     <GolfSections />
     <Footer /> </div>;
   }
