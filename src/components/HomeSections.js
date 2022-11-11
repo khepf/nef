13820829309c89030c7d07bdbebb7d0b56eeceae
Jeremy Kepf <jeremy1975@gmail.com>
2022-11-10 20:19:31 -0500

    test cache 6

--- src/components/HomeSections.js
+++ src/components/HomeSections.js
@@ -10,7 +10,7 @@ class HomeSections extends Component {
     return (<Fragment>
       <section className="Section1">
       <div className="Section1-1">
-        <h1>Lance Corporal Nicholas Erdy was honored and proud to serve in the United States Marine Corps...</h1>
+        <h1>Lance Corporal Nicholas Erdy was honored and proud to serve in the United States Marine Corps.</h1>
         <p className="lead">He had dreamed of being in the military all his life and wanted nothing more than to be in Iraq where he could help his Marine brothers.</p>
         <p className="lead">On May 11, 2005, Nick gave the greatest sacrifice of all - his life. During Operation Matador, Nick was on a mission in the Al Anbar Province of Iraq when his Armored Assault Vehicle was hit by an I.E.D. He was only 21 years old on the day that he and five other Marines were killed in that attack. </p>
         <p className="lead">In Nick's memory, his family has created the Nick Erdy Foundation, a 501(C)(3) organization dedicated to benefit several scholarship funds and not-for-profit groups. Go to the About page to learn more on how your contributions have helped to give back. </p>
