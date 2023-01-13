caadfb2cc2874e179adf6ac63275996ef46558e2
Jeremy Kepf <jeremy1975@gmail.com>
2023-01-13 16:44:19 -0500

    new golf options and css tweaks

--- src/components/GolfSections.js
+++ src/components/GolfSections.js
@@ -1,63 +1,68 @@
-import React, { Component, Fragment } from "react";
+import React, { Component, Fragment } from 'react';
 
-import golf1 from "../img/golf1.jpg";
-import golf3 from "../img/hickoryWoods.jpg";
+import golf1 from '../img/golf1.jpg';
+import golf3 from '../img/hickoryWoods.jpg';
 // import golf11 from "../img/golf11.png";
 // import golf2222 from "../img/golf2222.png";
 
-import homeSection2pic from "../img/homeSection2pic.jpg";
-import golfPDF from "../img/2022GolfNEF.pdf";
+import homeSection2pic from '../img/homeSection2pic.jpg';
+import golfPDF from '../img/2022GolfNEF.pdf';
 
-import "../App.css";
+import '../App.css';
 
 class GolfSections extends Component {
   render() {
     return (
       <Fragment>
-        <section className="GolfSection1">
-          <div className="GolfSection1-1">
-            <h3>16th Annual 2022 Nick Erdy Foundation Golf Outing</h3>
-            {/* <p className="lead">Unfortunately, due to COVID-19, this year’s golf outing has been cancelled. </p>
-            <p className="lead"> Thanks to all of our supporters and we wish you health and safety until we meet again in 2021.</p> */}
-            <p className="lead">
-              will be held on June 4th, 2022 at the Hickory Woods Golf Course in
+        <section className='GolfSection1'>
+          <div className='GolfSection1-1'>
+            <h3>17th Annual 2023 Nick Erdy Foundation Golf Outing</h3>
+            <p className='lead'>
+              will be held on June 3rd, 2023 at the Hickory Woods Golf Course in
               Loveland. Registration is from 12 to 1pm with a shotgun start at
               1:30 pm. Cost of ticket also includes dinner and Two (2) drink
               tickets.
             </p>
-            <p className="lead">
+            <p className='lead'>
               Awards and prizes for 1st place, longest drive, closest to the pin
               and longest putt.
             </p>
-            <p className="lead">
+            <p className='lead'>
+              In an effort to simplify the check-in process this year, we’re
+              offering all the “extras” ahead of time in these convenient
+              packages! If you don’t see a package that suits your needs don’t
+              worry - all individual options will still be available the day of
+              the outing!
+            </p>
+            <p className='lead'>
               Checks can be mailed to address below or payment can now be made
               via PayPal. PayPal is fast, convenient and you don't need a PayPal
               account to use this method of payment. Let us know if you have any
               questions.
             </p>
-            <p className="lead">
+            <p className='lead'>
               It is the goal of this foundation to continue these works for many
               years to come. Through the generosity of so many wonderful people,
               both here at home, and all over the United States, we can continue
               to help others while keeping Nick's memory alive.
             </p>
           </div>
-          <div className="GolfSection1-2">
-            <img src={golf1} alt="Golf 1" className="Golf1pic" />
+          <div className='GolfSection1-2'>
+            <img src={golf1} alt='Golf 1' className='Golf1pic' />
             <h3>Additional Info</h3>
             <div>
-              <img src={golf3} alt="" />
+              <img src={golf3} alt='' />
             </div>
-            <div className="DeerTrackTextDiv">
-              <a href={golfPDF} target="_blank">
+            <div className='DeerTrackTextDiv'>
+              <a href={golfPDF} target='_blank'>
                 Golf Outing Registration Form
               </a>
             </div>
-            <div className="DeerTrackTextDiv">
+            <div className='DeerTrackTextDiv'>
               <a
-                href="http://www.hickorywoods.com/directions"
-                target="_blank"
-                rel="noopener noreferrer"
+                href='http://www.hickorywoods.com/directions'
+                target='_blank'
+                rel='noopener noreferrer'
               >
                 Directions to course
               </a>
@@ -67,159 +72,258 @@ class GolfSections extends Component {
             <p>Tel: 513-575-3900</p>
           </div>
         </section>
-        <section className="GolfSection2">
-          <div className="GolfSection2-1 tester">
+        <section className='GolfSection2'>
+          <div className='GolfSection2-1 tester'>
             {/* <img src={golf11} alt=""/> */}
             <h3>Individual Golfer - $100</h3>
-            <p class="lead">list team name or other golfers in your group.</p>
+            <p class='lead'>list team name or other golfers in your group</p>
             <form
-              align="center"
-              action="https://www.paypal.com/cgi-bin/webscr"
-              method="post"
-              target="_top"
+              align='center'
+              action='https://www.paypal.com/cgi-bin/webscr'
+              method='post'
+              target='_top'
             >
-              <input name="cmd" type="hidden" value="_s-xclick" />{" "}
+              <input name='cmd' type='hidden' value='_s-xclick' />{' '}
               <input
-                name="hosted_button_id"
-                type="hidden"
-                value="Q2LQN5UESDVP4"
-              />{" "}
+                name='hosted_button_id'
+                type='hidden'
+                value='Q2LQN5UESDVP4'
+              />{' '}
               <input
-                alt="PayPal - The safer, easier way to pay online!"
-                border="0"
-                name="submit"
-                src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
-                type="image"
-              />{" "}
+                alt='PayPal - The safer, easier way to pay online!'
+                border='0'
+                name='submit'
+                src='https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif'
+                type='image'
+              />{' '}
               <img
-                alt=""
-                border="0"
-                height="1"
-                src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"
-                width="1"
+                alt=''
+                border='0'
+                height='1'
+                src='https://www.paypalobjects.com/en_US/i/scr/pixel.gif'
+                width='1'
               />
             </form>
           </div>
-          <div className="GolfSection2-2 tester">
-            {/* <img src={golf2222} alt="" style={{ "width": "95%"}}/> */}
-            <h3>Golf Outing Foursome - $400</h3>
-            <p className="lead">
-              make sure to list all 4 golfers on the donate screen.
+          <div className='GolfSection2-2 tester'>
+            <h3>Bronze - $400</h3>
+            <h4>Golf Outing Foursome</h4>
+            <p className='lead'>
+              make sure to list all 4 golfers on the donate screen
             </p>
             <form
-              align="center"
-              action="https://www.paypal.com/cgi-bin/webscr"
-              method="post"
-              target="_top"
+              action='https://www.paypal.com/donate'
+              method='post'
+              target='_top'
             >
-              <input name="cmd" type="hidden" value="_s-xclick" />{" "}
               <input
-                name="hosted_button_id"
-                type="hidden"
-                value="TQGQSZ3VKGLCC"
-              />{" "}
+                type='hidden'
+                name='hosted_button_id'
+                value='TQGQSZ3VKGLCC'
+              />
               <input
-                alt="PayPal - The safer, easier way to pay online!"
-                border="0"
-                name="submit"
-                src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
-                type="image"
-              />{" "}
+                type='image'
+                src='https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif'
+                border='0'
+                name='submit'
+                title='PayPal - The safer, easier way to pay online!'
+                alt='Donate with PayPal button'
+              />
               <img
-                alt=""
-                border="0"
-                height="1"
-                src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"
-                width="1"
+                alt=''
+                border='0'
+                src='https://www.paypal.com/en_US/i/scr/pixel.gif'
+                width='1'
+                height='1'
+              />
+            </form>
+          </div>
+          <div className='GolfSection2-2 tester'>
+            <h3>Silver - $440</h3>
+            <h4>Golf Outing Foursome + 4 Mulligans</h4>
+            <p className='lead'>
+              make sure to list all 4 golfers on the donate screen
+            </p>
+            <form
+              action='https://www.paypal.com/donate'
+              method='post'
+              target='_top'
+            >
+              <input
+                type='hidden'
+                name='hosted_button_id'
+                value='P9HE69JMEDJEC'
+              />
+              <input
+                type='image'
+                src='https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif'
+                border='0'
+                name='submit'
+                title='PayPal - The safer, easier way to pay online!'
+                alt='Donate with PayPal button'
+              />
+              <img
+                alt=''
+                border='0'
+                src='https://www.paypal.com/en_US/i/scr/pixel.gif'
+                width='1'
+                height='1'
+              />
+            </form>
+          </div>
+          <div className='GolfSection2-2 tester'>
+            <h3>Gold - $460</h3>
+            <h4>Golf Outing Foursome + 4 Mulligans</h4>
+            <h4>+ Split the pot</h4>
+            <p className='lead'>
+              make sure to list all 4 golfers on the donate screen
+            </p>
+            <form
+              action='https://www.paypal.com/donate'
+              method='post'
+              target='_top'
+            >
+              <input
+                type='hidden'
+                name='hosted_button_id'
+                value='AC4TFCB2VHK38'
+              />
+              <input
+                type='image'
+                src='https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif'
+                border='0'
+                name='submit'
+                title='PayPal - The safer, easier way to pay online!'
+                alt='Donate with PayPal button'
+              />
+              <img
+                alt=''
+                border='0'
+                src='https://www.paypal.com/en_US/i/scr/pixel.gif'
+                width='1'
+                height='1'
+              />
+            </form>
+          </div>
+          <div className='GolfSection2-2 tester'>
+            <h3>Platinum - $500</h3>
+            <h4>Golf Outing Foursome + 4 Mulligans</h4>
+            <h4>+ Split the pot</h4>
+            <h4>+ Team entry for all game/contest holes</h4>
+            <p className='lead'>
+              make sure to list all 4 golfers on the donate screen
+            </p>
+            <form
+              action='https://www.paypal.com/donate'
+              method='post'
+              target='_top'
+            >
+              <input
+                type='hidden'
+                name='hosted_button_id'
+                value='35FM3N8SCKMPY'
+              />
+              <input
+                type='image'
+                src='https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif'
+                border='0'
+                name='submit'
+                title='PayPal - The safer, easier way to pay online!'
+                alt='Donate with PayPal button'
+              />
+              <img
+                alt=''
+                border='0'
+                src='https://www.paypal.com/en_US/i/scr/pixel.gif'
+                width='1'
+                height='1'
               />
             </form>
           </div>
 
-          <div className="GolfSection2-3">
+          <div className='GolfSection2-3'>
             <h3>Individual Dinner - $15</h3>
-            <p className="lead">reservation without the golf</p>
+            <p className='lead'>reservation without the golf</p>
             <form
-              align="center"
-              action="https://www.paypal.com/cgi-bin/webscr"
-              method="post"
-              target="_top"
+              align='center'
+              action='https://www.paypal.com/cgi-bin/webscr'
+              method='post'
+              target='_top'
             >
-              <input name="cmd" type="hidden" value="_s-xclick" />{" "}
+              <input name='cmd' type='hidden' value='_s-xclick' />{' '}
               <input
-                name="hosted_button_id"
-                type="hidden"
-                value="C65CPMRPUR9LL"
-              />{" "}
+                name='hosted_button_id'
+                type='hidden'
+                value='C65CPMRPUR9LL'
+              />{' '}
               <input
-                alt="PayPal - The safer, easier way to pay online!"
-                border="0"
-                name="submit"
-                src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
-                type="image"
-              />{" "}
+                alt='PayPal - The safer, easier way to pay online!'
+                border='0'
+                name='submit'
+                src='https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif'
+                type='image'
+              />{' '}
               <img
-                alt=""
-                border="0"
-                height="1"
-                src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"
-                width="1"
+                alt=''
+                border='0'
+                height='1'
+                src='https://www.paypalobjects.com/en_US/i/scr/pixel.gif'
+                width='1'
               />
             </form>
           </div>
-          <div className="GolfSection2-4">
+          <div className='GolfSection2-4'>
             <h3>1 Hole Sponsorship - $100</h3>
-            <p className="lead">a fun way to donate</p>
+            <p className='lead'>a fun way to donate!</p>
             <form
-              align="center"
-              action="https://www.paypal.com/cgi-bin/webscr"
-              method="post"
-              target="_top"
+              align='center'
+              action='https://www.paypal.com/cgi-bin/webscr'
+              method='post'
+              target='_top'
             >
-              <input name="cmd" type="hidden" value="_s-xclick" />{" "}
+              <input name='cmd' type='hidden' value='_s-xclick' />{' '}
               <input
-                name="hosted_button_id"
-                type="hidden"
-                value="ZVTVFJNSKM7S2"
-              />{" "}
+                name='hosted_button_id'
+                type='hidden'
+                value='ZVTVFJNSKM7S2'
+              />{' '}
               <input
-                alt="PayPal - The safer, easier way to pay online!"
-                border="0"
-                name="submit"
-                src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif"
-                type="image"
-              />{" "}
+                alt='PayPal - The safer, easier way to pay online!'
+                border='0'
+                name='submit'
+                src='https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif'
+                type='image'
+              />{' '}
               <img
-                alt=""
-                border="0"
-                height="1"
-                src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif"
-                width="1"
+                alt=''
+                border='0'
+                height='1'
+                src='https://www.paypalobjects.com/en_US/i/scr/pixel.gif'
+                width='1'
               />
             </form>
           </div>
         </section>
 
-        <section className="GolfSection3"></section>
-
-        <section className="AboutSection2">
-          <div className="AboutSection2-1">
+        <section className='AboutSection2'>
+          <div className='AboutSection2-1'>
             <h3>Foundation Information</h3>
-            <p className="lead">Address: 8281 SR 134</p>
-            <p className="lead">Lynchburg, OH 45142</p>
-            <p className="lead">Phone: (937) 579-5200</p>
-            <p className="GoogleFormsDiv lead">
+            <p className='lead'>Address: 8281 SR 134</p>
+            <p className='lead'>Lynchburg, OH 45142</p>
+            <p className='lead'>Phone: (937) 579-5200</p>
+            <p className='GoogleFormsDiv lead'>
               <a
-                href="https://docs.google.com/forms/d/1SvMZjgUEbKHQXzh2dMh5fm3Df_FhCbMN8jVZ7b1dCTY"
-                target="_blank"
-                rel="noopener noreferrer"
+                href='https://docs.google.com/forms/d/1SvMZjgUEbKHQXzh2dMh5fm3Df_FhCbMN8jVZ7b1dCTY'
+                target='_blank'
+                rel='noopener noreferrer'
               >
                 Contact Form
               </a>
             </p>
-            <p className="lead">501(c)(3) #51-0619676</p>
+            <p className='lead'>501(c)(3) #51-0619676</p>
           </div>
-          <div className="AboutSection2-2">
-            <img src={homeSection2pic} alt="" />
+          <div className='AboutSection2-2'>
+            <img src={homeSection2pic} alt='' />
           </div>
         </section>
       </Fragment>
