c9f6b24a5e46bd13912ffdbccf224b94e9003f64
Jeremy Kepf <jeremy1975@gmail.com>
2023-01-14 08:53:05 -0500

    headers file test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <div className="Footer">
+      <div className='Footer'>
         <footer>
-          <p className="FooterP lead">
-            &copy; { new Date().getFullYear() } Nick Erdy Foundation | All rights reserved.
+          <p className='FooterP lead'>
+            &copy; {new Date().getFullYear()} Nick Erdy Foundation | All rights
+            reserved
           </p>
         </footer>
       </div>
