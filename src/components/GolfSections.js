a308cbb3826cdd0a375f886494ffb950bc31d2a1
Jeremy Kepf <jeremy1975@gmail.com>
2023-01-22 08:26:40 -0500

    remove unnedded import

--- src/components/GolfSections.js
+++ src/components/GolfSections.js
@@ -4,7 +4,6 @@ import golf1 from '../img/golf1.jpg';
 import golf3 from '../img/hickoryWoods.jpg';
 
 import homeSection2pic from '../img/homeSection2pic.jpg';
-import golfPDF from '../img/2022GolfNEF.pdf';
 import golf2023 from '../img/2023golf.pdf';
 
 import '../App.css';
