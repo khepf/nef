d27e12c31fe187c38c29395ee94cbae8392b4a34
Jeremy Kepf <jeremy1975@gmail.com>
2023-01-22 08:45:07 -0500

    change benefit dtates to 2023

--- src/App.js
+++ src/App.js
@@ -8,15 +8,20 @@ import Contact from './components/Contact';
 import Donate from './components/Donate';
 import Golf from './components/Golf';
 import Home from './components/Home';
+
+// 2023
 class App extends Component {
   render() {
-    return <div className="App">
-    <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/benefit" component={Benefit} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/donate" component={Donate} />
-      <Route path="/golf" component={Golf} /> </div>;
+    return (
+      <div className='App'>
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/benefit' component={Benefit} />
+        <Route path='/contact' component={Contact} />
+        <Route path='/donate' component={Donate} />
+        <Route path='/golf' component={Golf} />{' '}
+      </div>
+    );
   }
 }
 
